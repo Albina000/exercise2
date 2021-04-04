b08afbcdd78725645e8a651de2af4e1a204bf325
Albina Sidorova <albina.pvp@mail.ru>
2021-04-04 19:41:18 +0500

    Реализация 5 теста

--- src/Authorization.js
+++ src/Authorization.js
@@ -7,6 +7,9 @@ class Authorization {
         if (str.length < 17) {
             throw new Error("Токен меньше 10 символов"); 
         }
+        if (!str.startsWith('Bearer ')) {
+            throw new Error("Недопустимый префикс");
+        }
         const token = str.slice(7);
         if(token.search(/^[A-Za-z0-9]+$/) === -1){
             throw new Error("Недопустимый символ"); 
