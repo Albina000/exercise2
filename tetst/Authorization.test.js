e8f1f1532edb4faed3531abb9c573ef96ce46747
Albina Sidorova <albina.pvp@mail.ru>
2021-04-04 17:30:59 +0500

    first commit

---
+++ tetst/Authorization.test.js
@@ -0,0 +1,16 @@
+const {Authorization} = require('../src/Authorization.js');
+ 
+ test ('Правильный токен', () => {
+    //Arrange
+    let auth = new Authorization();
+
+    const inputString = 'Bearer vCVVCHjTylu2lMtEf5wjljd5VvBkuizKYGITwAoV75XXj5uiv30MLYtINF';
+    const expectedToken = 'vCVVCHjTylu2lMtEf5wjljd5VvBkuizKYGITwAoV75XXj5uiv30MLYtINF';
+
+    //Act
+    const token = auth.getToken(inputString);
+
+    //Assert
+    expect(token).toBe(expectedToken);
+
+});
