f00d9d20f811cd3498a30cbf9f9489ed27b6f3bd
Albina Sidorova <albina.pvp@mail.ru>
2021-04-04 19:26:36 +0500

    Реализация 4 теста

--- src/Authorization.js
+++ src/Authorization.js
@@ -7,7 +7,12 @@ class Authorization {
         if (str.length < 17) {
             throw new Error("Токен меньше 10 символов"); 
         }
-        return str.slice(7);
+        const token = str.slice(7);
+        if(token.search(/^[A-Za-z0-9]+$/) === -1){
+            throw new Error("Недопустимый символ"); 
+        }
+        return token;
+    
     }
 }
 
